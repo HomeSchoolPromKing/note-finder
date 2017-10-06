60798b57b9f7a1b8eebd8930da0a776a07e9feaa
HomeSchoolPromKing <elliottzb@gmail.com>
2017-10-05 22:50:22 -0400

    Restructured songBuilderView to make more sense

--- js/songBuilderView.js
+++ js/songBuilderView.js
@@ -130,150 +130,147 @@ var addBlockDiv = function() {
     //Defaults to 0 if not called by a button
     var index = $(this.parentNode).index();
     console.log(index);
-    if (index === -1) {
-        console.log("Index was undefined. This should only happen once");
-        index = 0;
+    console.log("new index: " + (index + 1));
+        
+    //Iterates through exisitng divs after this one and changes indices
+    for (i = (index + 1); i < (currentSong.blockArray.length + 1); i++) {
+        $('#songBlock' + (i + 1)).id = ("songBlock" + (i + 2));
+        $('#chordDisplay' + (i + 1)).id = ("chordDisplay" + (i + 2));
+        $('#notesDisplay' + (i + 1)).id = ("notesDisplay" + (i + 2));
+        $('#lyricsDisplay' + (i + 1)).id = ("lyricsDisplay" + (i + 2));
+        $('#rootSelect' + (i + 1)).id = ("rootSelect" + (i + 2));
+        $('#chordSelect' + (i + 1)).id = ("chordSelect" + (i + 2));
+        $('#lyricsBox' + (i + 1)).id = ("lyricsBox" + (i + 2));
+        $('#beatsSelect' + (i + 1)).id = ("beatsSelect" + (i + 2));
+        $('#btnAddBlock' + (i + 1)).id = ("btnAddBlcok" + (i + 2));
     }
-    if (currentSong.blockArray.length === 0 && index === 0) {
-        $("#songBlocksContainer").append(
-            '<div id="songBlock'+ index +'" class="songBlock4" style="border-style: solid;">\
-                <h3 id="chordDisplay' + index +'"></h3>\
-                <h4 id="notesDisplay' + index +'"></h4>\
-                <h4 id="lyricsDisplay' + index +'"></h4>\
-                <label for="rootSelect' + index +'">Select Root:</label>\
-                <select id="rootSelect' + index +'" class="rootSelect" onchange="onRootChange">\
-                    <option value="A">A</option>\
-                    <option value="A#/Bb">A#/Bb</option>\
-                    <option value="B">B</option>\
-                    <option value="C">C</option>\
-                    <option value="C#/Db">C#/Db</option>\
-                    <option value="D">D</option>\
-                    <option value="D#/Eb">D#/Eb</option>\
-                    <option value="E">E</option>\
-                    <option value="F">F</option>\
-                    <option value="F#/Gb">F#/Gb</option>\
-                    <option value="G">G</option>\
-                    <option value="G#/Ab">G#/Ab</option>\
-                </select>\
-                <label for="chordSelect' + index +'">Select Chord Type:</label>\
-                <select id="chordSelect' + index +'" class="chordSelect" onchange="onChordChange">\
-                    <!--View fills options.-->\
-                </select>\
-                <br>\
-                <label for="lyricsBox' + index +'">Lyrics: </label>\
-                <input type="text" id="lyricsBox' + index +'" class="lyricBox">\
-                <label for="beatsSelect' + index +'">Beats: </label> \
-                <select id="beatsSelect' + index +'" onchange="onBeatsChange">\
-                    <option value="1">1</option>\
-                    <option value="2">2</option>\
-                    <option value="3">3</option>\
-                    <option value="4" selected="selected">4</option>\
-                </select>\
-                <input type="button" id="btnAddBlock' + index +'" class="btnAddBlock" onclick="addBlockDiv" value="Add Block">\n\
-            </div>');
-        //Populate chord type select options
-        for (i = 0; i < chordList.length; i++){
-            //console.log('select.chordSelect: ' + index);
-            $('#chordSelect' + index).append($("<option></option>")
-                    .attr("value", chordList[i].type)
-                    .text(chordList[i].type));
-            //console.log("Option populated, boss");
-        };
-        //Add listeners
-        $('#rootSelect' + index).change(onRootChange);
-        $('#chordSelect' + index).change(onChordChange);
-        $('#beatsSelect' + index).change(onBeatsChange);
-        $('#btnAddBlock' + index).click(addBlockDiv);
-        $('#lyricsBox' + index).on('input', onLyricsChange);
-        
-        //Add block to song object
-        currentSong.insertBlock(0);
-        
-        //Initialize chord output
-        updateChordOutput(index, currentSong.blockArray[index].chord);
-    }
-    else {
-        
-        //Iterates through exisitng divs after this one and changes indices
-        for (i = (index + 1); i < (currentSong.blockArray.length + 1); i++) {
-            $('#songBlock' + (i + 1)).id = ("songBlock" + (i + 2));
-            $('#chordDisplay' + (i + 1)).id = ("chordDisplay" + (i + 2));
-            $('#notesDisplay' + (i + 1)).id = ("notesDisplay" + (i + 2));
-            $('#lyricsDisplay' + (i + 1)).id = ("lyricsDisplay" + (i + 2));
-            $('#rootSelect' + (i + 1)).id = ("rootSelect" + (i + 2));
-            $('#chordSelect' + (i + 1)).id = ("chordSelect" + (i + 2));
-            $('#lyricsBox' + (i + 1)).id = ("lyricsBox" + (i + 2));
-            $('#beatsSelect' + (i + 1)).id = ("beatsSelect" + (i + 2));
-            $('#btnAddBlock' + (i + 1)).id = ("btnAddBlcok" + (i + 2));
-        }
-        
-        $("#songBlock" + index).after(
-            '<div id="songBlock'+ (index + 1) +'" class="songBlock4" style="border-style: solid;">\
-                <h3 id="chordDisplay' + (index + 1) +'"></h3>\
-                <h4 id="notesDisplay' + (index + 1) +'"></h4>\
-                <h4 id="lyricsDisplay' + (index + 1) +'"></h4>\
-                <label for="rootSelect' + (index + 1) +'">Select Root:</label>\
-                <select id="rootSelect' + (index + 1) +'" class="rootSelect" onchange="onRootChange">\
-                    <option value="A">A</option>\
-                    <option value="A#/Bb">A#/Bb</option>\
-                    <option value="B">B</option>\
-                    <option value="C">C</option>\
-                    <option value="C#/Db">C#/Db</option>\
-                    <option value="D">D</option>\
-                    <option value="D#/Eb">D#/Eb</option>\
-                    <option value="E">E</option>\
-                    <option value="F">F</option>\
-                    <option value="F#/Gb">F#/Gb</option>\
-                    <option value="G">G</option>\
-                    <option value="G#/Ab">G#/Ab</option>\
-                </select>\
-                <label for="chordSelect' + (index + 1) +'">Select Chord Type:</label>\
-                <select id="chordSelect' + (index + 1) +'" class="chordSelect" onchange="onChordChange">\
-                    <!--View fills options.-->\
-                </select>\
-                <br>\
-                <label for="lyricsBox' + (index + 1) +'">Lyrics: </label>\
-                <input type="text" id="lyricsBox' + (index + 1) +'" class="lyricBox">\
-                <label for="beatsSelect' + (index + 1) +'">Beats: </label> \
-                <select id="beatsSelect' + (index + 1) +'" onchange="onBeatsChange">\
-                    <option value="1">1</option>\
-                    <option value="2">2</option>\
-                    <option value="3">3</option>\
-                    <option value="4" selected="selected">4</option>\
-                </select>\
-                <input type="button" id="btnAddBlock' + (index + 1) +'" class="btnAddBlock" onclick="addBlockDiv" value="Add Block">\n\
-            </div>');
-        //Populate chord type select options
-        for (i = 0; i < chordList.length; i++){
-            //console.log('select.chordSelect: ' + (index + 1);
-            $('#chordSelect' + (index + 1)).append($("<option></option>")
-                    .attr("value", chordList[i].type)
-                    .text(chordList[i].type));
-            //console.log("Option populated, boss");
-        };
-        
-        //Add listeners
-        $('#rootSelect' + (index +1)).change(onRootChange);
-        $('#chordSelect' + (index +1)).change(onChordChange);
-        $('#beatsSelect' + (index +1)).change(onBeatsChange);
-        $('#btnAddBlock' + (index +1)).click(addBlockDiv);
-        $('#lyricsBox' + (index +1)).on('input', onLyricsChange);
-        
-        //Insert block in song object
-        currentSong.insertBlock(index + 1);
-        
-        //Initialize chord output
-        updateChordOutput((index + 1), currentSong.blockArray[index + 1].chord);
-    }
-    updateChordsUsedView();
     
-    //I done fucked up now
-    debugSong();
+    console.log("y tho");
+    $("#songBlock" + index).after(
+        '<div id="songBlock'+ (index + 1) +'" class="songBlock4" style="border-style: solid;">\
+            <h3 id="chordDisplay' + (index + 1) +'"></h3>\
+            <h4 id="notesDisplay' + (index + 1) +'"></h4>\
+            <h4 id="lyricsDisplay' + (index + 1) +'"></h4>\
+            <label for="rootSelect' + (index + 1) +'">Select Root:</label>\
+            <select id="rootSelect' + (index + 1) +'" class="rootSelect" onchange="onRootChange">\
+                <option value="A">A</option>\
+                <option value="A#/Bb">A#/Bb</option>\
+                <option value="B">B</option>\
+                <option value="C">C</option>\
+                <option value="C#/Db">C#/Db</option>\
+                <option value="D">D</option>\
+                <option value="D#/Eb">D#/Eb</option>\
+                <option value="E">E</option>\
+                <option value="F">F</option>\
+                <option value="F#/Gb">F#/Gb</option>\
+                <option value="G">G</option>\
+                <option value="G#/Ab">G#/Ab</option>\
+            </select>\
+            <label for="chordSelect' + (index + 1) +'">Select Chord Type:</label>\
+            <select id="chordSelect' + (index + 1) +'" class="chordSelect" onchange="onChordChange">\
+                <!--View fills options.-->\
+            </select>\
+            <br>\
+            <label for="lyricsBox' + (index + 1) +'">Lyrics: </label>\
+            <input type="text" id="lyricsBox' + (index + 1) +'" class="lyricBox">\
+            <label for="beatsSelect' + (index + 1) +'">Beats: </label> \
+            <select id="beatsSelect' + (index + 1) +'" onchange="onBeatsChange">\
+                <option value="1">1</option>\
+                <option value="2">2</option>\
+                <option value="3">3</option>\
+                <option value="4" selected="selected">4</option>\
+            </select>\
+            <input type="button" id="btnAddBlock' + (index + 1) +'" class="btnAddBlock" onclick="addBlockDiv" value="Add Block">\n\
+        </div>');
+    
+    //Populate chord type select options
+    for (i = 0; i < chordList.length; i++){
+        //console.log('select.chordSelect: ' + (index + 1);
+        $('#chordSelect' + (index + 1)).append($("<option></option>")
+                .attr("value", chordList[i].type)
+                .text(chordList[i].type));
+        //console.log("Option populated, boss");
+    };
+
+    //Add listeners
+    $('#rootSelect' + (index +1)).change(onRootChange);
+    $('#chordSelect' + (index +1)).change(onChordChange);
+    $('#beatsSelect' + (index +1)).change(onBeatsChange);
+    $('#btnAddBlock' + (index +1)).click(addBlockDiv);
+    $('#lyricsBox' + (index +1)).on('input', onLyricsChange);
+
+    //Insert block in song object
+    currentSong.insertBlock(index + 1);
+
+    //Initialize chord output
+    updateChordOutput((index + 1), currentSong.blockArray[index + 1].chord);
+    updateChordsUsedView();
 };
 
 //Create first block on ready
 $(document).ready(function (){    
+    
     //Creat first block
-    addBlockDiv();
+    $("#songBlocksContainer").append(
+        '<div id="songBlock0" class="songBlock4" style="border-style: solid;">\
+            <h3 id="chordDisplay0"></h3>\
+            <h4 id="notesDisplay0"></h4>\
+            <h4 id="lyricsDisplay0"></h4>\
+            <label for="rootSelect0">Select Root:</label>\
+            <select id="rootSelect0" class="rootSelect" onchange="onRootChange">\
+                <option value="A">A</option>\
+                <option value="A#/Bb">A#/Bb</option>\
+                <option value="B">B</option>\
+                <option value="C">C</option>\
+                <option value="C#/Db">C#/Db</option>\
+                <option value="D">D</option>\
+                <option value="D#/Eb">D#/Eb</option>\
+                <option value="E">E</option>\
+                <option value="F">F</option>\
+                <option value="F#/Gb">F#/Gb</option>\
+                <option value="G">G</option>\
+                <option value="G#/Ab">G#/Ab</option>\
+            </select>\
+            <label for="chordSelect0">Select Chord Type:</label>\
+            <select id="chordSelect0" class="chordSelect" onchange="onChordChange">\
+                <!--View fills options.-->\
+            </select>\
+            <br>\
+            <label for="lyricsBox0">Lyrics: </label>\
+            <input type="text" id="lyricsBox0" class="lyricBox">\
+            <label for="beatsSelect0">Beats: </label> \
+            <select id="beatsSelect0" onchange="onBeatsChange">\
+                <option value="1">1</option>\
+                <option value="2">2</option>\
+                <option value="3">3</option>\
+                <option value="4" selected="selected">4</option>\
+            </select>\
+            <input type="button" id="btnAddBlock0" class="btnAddBlock" onclick="addBlockDiv" value="Add Block">\n\
+        </div>');
+    //Populate chord type select options
+    for (i = 0; i < chordList.length; i++){
+        //console.log('select.chordSelect: ' + index);
+        $('#chordSelect0').append($("<option></option>")
+                .attr("value", chordList[i].type)
+                .text(chordList[i].type));
+        //console.log("Option populated, boss");
+    };
+    //Add listeners
+    $('#rootSelect0').change(onRootChange);
+    $('#chordSelect0').change(onChordChange);
+    $('#beatsSelect0').change(onBeatsChange);
+    $('#btnAddBlock0').click(addBlockDiv);
+    $('#lyricsBox0').on('input', onLyricsChange);
+
+    //Add block to song object
+    currentSong.insertBlock(0);
+
+    //Initialize chord output
+    updateChordOutput(0, currentSong.blockArray[0].chord);
+    
+    //Initialize chords used
+    updateChordsUsedView();
+    
     //TODO: add listeners for song (non-songBlock-specific) options
+    
 });
